b424e256d46af5d7528e06f131b7a6aa60e107f4
Ali Pishvaee <alipishvaee2@Gmail.com>
2020-06-12 23:54:20 +0430

    SignUp ViewConrtoller

---
+++ CommunereTest/Views/SignupViewController.swift
@@ -0,0 +1,31 @@
+//
+//  SignupViewController.swift
+//  CommunereTest
+//
+//  Created by Ali Pishvaee on 6/12/20.
+//  Copyright © 2020 AliPishvaee. All rights reserved.
+//
+
+import UIKit
+
+class SignupViewController: UIViewController {
+
+    @IBOutlet weak var fullnameTextField: CustomTextField!
+    @IBOutlet weak var email_UserNameTextField: CustomTextField!
+    @IBOutlet weak var passwordTextField: CustomTextField!
+    @IBOutlet weak var confirmPasswordTextField: CustomTextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func signupButton(_ sender: Any) {
+    
+    }
+    
+    @IBAction func imageUploadButton(_ sender: Any) {
+    
+    }
+    
+    
+}
